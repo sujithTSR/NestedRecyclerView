fd5c2b22c340372f7d40509d428fc02a7cfb2acd
Sujith <sujitroyal.005@gmail.com>
2019-03-01 14:41:33 +0530

    First Commit

---
+++ app/src/main/java/com/tech/tsr/nestedrecyclerview/MainActivity.java
@@ -0,0 +1,14 @@
+git status
+        package com.tech.tsr.nestedrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
